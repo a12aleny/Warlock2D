d00d959e70f439765756ea8e6528979c11238dcb
a12aleny <a12aleny@student.his.se>
2014-09-14 04:19:59 +0200

    Skapade grundläggande struktur.
    Spelare skapas.
    Går att röra spelaren med mustangenterna.
    Implementerat en handler för musens position som kan ses i  konsolen,
    ska användas sedan för att få spelaren att röra sig via högerklick på
    musen istället för mustangenterna.
    Knappen G har en handler för att sedan kunna lägga till den första
    grundläggande spellen "fireball".

---
+++ public/js/game.js
@@ -0,0 +1,133 @@
+/**************************************************
+** GAME VARIABLES
+**************************************************/
+var canvas,			// Canvas DOM element
+	ctx,			// Canvas rendering context
+	keys,			// Keyboard input
+	localPlayer;	// Local player
+
+
+/**************************************************
+** GAME INITIALISATION
+**************************************************/
+function init() {
+	// Declare the canvas and rendering context
+	canvas = document.getElementById("gameCanvas");
+	ctx = canvas.getContext("2d");
+
+	// Maximise the canvas
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	// Initialise keyboard controls
+	keys = new Keys();
+
+	// Calculate a random start position for the local player
+	// The minus 5 (half a player size) stops the player being
+	// placed right on the egde of the screen
+	var startX = Math.round(Math.random()*(canvas.width-5)),
+		startY = Math.round(Math.random()*(canvas.height-5));
+
+	// Initialise the local player
+
+	localPlayer = new Player(startX, startY);
+
+	// Start listening for events
+	setEventHandlers();
+};
+
+
+/**************************************************
+** GAME EVENT HANDLERS
+**************************************************/
+var setEventHandlers = function() {
+	// Keyboard
+	window.addEventListener("keydown", onKeydown, false);
+	window.addEventListener("keyup", onKeyup, false);
+
+	// Window resize
+	window.addEventListener("resize", onResize, false);
+
+	//Prevent right mouse button 
+  	canvas.addEventListener('contextmenu', function(e) {
+      if (e.button === 2) {
+       e.preventDefault();
+        return false;
+      }
+  }, false);
+};
+
+//Print mouseposition
+function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
+      }
+	var canvas = document.getElementById('gameCanvas');
+      var context = canvas.getContext('2d');
+
+      canvas.addEventListener('mousemove', function(evt) {
+        var mousePos = getMousePos(canvas, evt);
+        console.log('Mouse position: ' + mousePos.x + ',' + mousePos.y);
+        
+      }, false);
+
+
+
+
+
+
+// Keyboard key down
+function onKeydown(e) {
+	if (localPlayer) {
+		keys.onKeyDown(e);
+	};
+};
+
+// Keyboard key up
+function onKeyup(e) {
+	if (localPlayer) {
+		keys.onKeyUp(e);
+	};
+};
+
+// Browser window resize
+function onResize(e) {
+	// Maximise the canvas
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+};
+
+
+/**************************************************
+** GAME ANIMATION LOOP
+**************************************************/
+function animate() {
+	update();
+	draw();
+
+	// Request a new animation frame using Paul Irish's shim
+	window.requestAnimFrame(animate);
+};
+
+
+/**************************************************
+** GAME UPDATE
+**************************************************/
+function update() {
+	localPlayer.update(keys);
+};
+
+
+/**************************************************
+** GAME DRAW
+**************************************************/
+function draw() {
+	// Wipe the canvas clean
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	// Draw the local player
+	localPlayer.draw(ctx);
+};
